8624c049044fbd398bcec80aa60164f5a92f7683
王泳淇 <815099246@qq.com>
2024-05-21 19:27:17 +0800

    commit

--- lib/flutter_ecosed.dart
+++ lib/flutter_ecosed.dart
@@ -3,10 +3,10 @@ library flutter_ecosed;
 import 'src/platform/ecosed_platform_interface.dart';
 import 'src/platform/flutter_ecosed_platform.dart';
 
-export 'src/app/entry.dart';
-export 'src/app/app.dart';
+export 'src/entry/entry.dart';
 export 'src/plugin/plugin.dart';
 export 'src/executor/executor.dart';
+export 'src/app/app.dart';
 
 final class FlutterEcosed {
   const FlutterEcosed();
