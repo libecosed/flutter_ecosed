3fdbeeced87c72be18a46b45beafc6b4ec8d0e78
王泳淇 <815099246@qq.com>
2024-05-30 17:20:57 +0800

    commit

--- src/flutter_ecosed.cpp
+++ src/flutter_ecosed.cpp
@@ -1,22 +1,17 @@
 #include "flutter_ecosed.h"
 
-// A very short-lived native function.
-//
-// For very short-lived functions, it is fine to call them on the main isolate.
-// They will block the Dart execution while running the native function, so
-// only do this for native functions which are guaranteed to be short-lived.
-extern "C"
-FFI_PLUGIN_EXPORT int sum(int a, int b) {
+int main() {
+
+  return 0;
+}
+
+extern "C" FFI_PLUGIN_EXPORT int sum(int a, int b)
+{
   return a + b;
 }
 
-// A longer-lived native function, which occupies the thread calling it.
-//
-// Do not call these kind of native functions in the main isolate. They will
-// block Dart execution. This will cause dropped frames in Flutter applications.
-// Instead, call these native functions on a separate isolate.
-extern "C"
-FFI_PLUGIN_EXPORT int sum_long_running(int a, int b) {
+extern "C" FFI_PLUGIN_EXPORT int sum_long_running(int a, int b)
+{
   // Simulate work.
 #if _WIN32
   Sleep(5000);
--- src/flutter_ecosed.h
+++ src/flutter_ecosed.h
@@ -1,7 +1,3 @@
-// #include <cstdint>
-// #include <cstdio>
-// #include <cstdlib>
-
 #ifdef __cplusplus
 #include <cstdint>
 #include <cstdio>
@@ -21,33 +17,14 @@
 #define FFI_PLUGIN_EXPORT
 #endif
 
-
-//静态库文件的头文件代码
-#ifdef __cplusplus          //检测是否为C++文件调用，若是则extern不起作用
-extern "C" {
+#ifdef __cplusplus
+extern "C"
+{
 #endif
 
-// A very short-lived native function.
-//
-// For very short-lived functions, it is fine to call them on the main isolate.
-// They will block the Dart execution while running the native function, so
-// only do this for native functions which are guaranteed to be short-lived.
-//extern "C"
-//__attribute__((visibility("default"))) __attribute__((used))
-FFI_PLUGIN_EXPORT int sum(int a, int b);
+    FFI_PLUGIN_EXPORT int sum(int a, int b);
+    FFI_PLUGIN_EXPORT int sum_long_running(int a, int b);
 
-// A longer lived native function, which occupies the thread calling it.
-//
-// Do not call these kind of native functions in the main isolate. They will
-// block Dart execution. This will cause dropped frames in Flutter applications.
-// Instead, call these native functions on a separate isolate.
-//extern "C"
-//__attribute__((visibility("default"))) __attribute__((used))
-FFI_PLUGIN_EXPORT int sum_long_running(int a, int b);
-	
-#ifdef __cplusplus  
+#ifdef __cplusplus
 }
 #endif
-
-
-
