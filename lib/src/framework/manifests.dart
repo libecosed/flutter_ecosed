552563aeed0973cb9089682250791e2324254767
王泳淇 <815099246@qq.com>
2024-07-13 20:58:37 +0800

    commit

--- lib/src/framework/manifests.dart
+++
@@ -1,8 +0,0 @@
-import 'a.dart';
-import 'activity.dart';
-import 'service.dart';
-
-class EcosedManifests {
-  static final List<Activity> activitys = <Activity>[MainActivity()];
-  static final List<Service> services = <Service>[MainService()];
-}
