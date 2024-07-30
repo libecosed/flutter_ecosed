93c248eaa5e0df96ab07dec23d48fd990bce9beb
wyq0918dev <wyq0918dev@gmail.com>
2024-07-30 12:03:36 +0800

    commit

--- ios/Classes/FlutterEcosedPlugin.swift
+++ ios/Classes/FlutterEcosedPlugin.swift
@@ -2,18 +2,18 @@ import Flutter
 import UIKit
 
 public class FlutterEcosedPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_ecosed", binaryMessenger: registrar.messenger())
-    let instance = FlutterEcosedPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_ecosed", binaryMessenger: registrar.messenger())
+        let instance = FlutterEcosedPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
